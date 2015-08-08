1ace055b5834805f63e4414e9c7c926e85a4da15
Joshua Moody <joshuajmoody@gmail.com>
2015-08-08 18:42:25 +0200

    Stand up the Xcode project

---
+++ iPhoneOnly/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iPhoneOnly
+//
+//  Created by Joshua Moody on 8.8.15.
+//  Copyright (c) 2015 Calabash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
